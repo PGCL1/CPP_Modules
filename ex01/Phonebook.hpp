9a5243d76a320a3fca052fc45fe952a66b1c56e6
PGCL1 <earlybirdcatchestheworm1@gmail.com>
2024-03-01 18:31:07 +0100

    compilation works now

--- ex01/Phonebook.hpp
+++ ex01/Phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: glacroix <glacroix@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 18:32:43 by glacroix          #+#    #+#             */
-/*   Updated: 2024/02/23 18:41:43 by glacroix         ###   ########.fr       */
+/*   Updated: 2024/03/01 18:26:46 by glacroix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,16 @@
 #define PHONEBOOK_HPP
 
 #include "Contact.hpp"
+#include <iomanip>
 
-class PhoneBook {
+class Phonebook {
 	Contact	Contacts[8];
 	int		contactCount;
 	public:
-		void addContact(int contactCount);
-		void searchContact();
-		void exitPhoneBook();
+		void	addContact(int contactCount);
+		void	searchContact(int &count);
+		void	exitPhoneBook();
+		int		executeInput(std::string input, int &count);
 };
 
 #endif

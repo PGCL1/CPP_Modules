679c51b035579d6d99a04d2ff6d4b83ebaa4e1f8
PGCL1 <earlybirdcatchestheworm1@gmail.com>
2024-03-12 10:42:07 +0100

    understood the necessity of multiple constructors

--- 01/ex01/Zombie.hpp
+++ 01/ex01/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: glacroix <glacroix@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 16:52:19 by glacroix          #+#    #+#             */
-/*   Updated: 2024/03/11 18:20:45 by glacroix         ###   ########.fr       */
+/*   Updated: 2024/03/12 10:40:47 by glacroix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ class Zombie {
 	void announce(void);
 	void nameZombie(std::string name);
 
-	//no fucking idea why
 	Zombie();
 	Zombie(std::string name);
 	~Zombie();
@@ -34,28 +33,3 @@ Zombie* newZombie( std::string name );
 Zombie* zombieHorde(int N, std::string name);
 
 #endif
-
-
-/*
-#include<iostream>
-
-using namespace std;
-
-class MyClass
-{
-	public:
-	void method(int i){
-		cout<<"object "<<i+1<<endl;}
-	};
-
-int main(){
-
-MyClass *array_of_objects= new MyClass[5]; // Creating an array of MyClass objects using 'new'
-
-for(int i=0; i<5; i++){
-
-array_of_objects[i].method(i);}
-
-delete[] array_of_objects;
-
-}*/

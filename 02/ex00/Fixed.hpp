ca8dbf1aa24daa790ef74760f3bd96c14d4a7a40
PGCL1 <earlybirdcatchestheworm1@gmail.com>
2024-05-03 19:10:57 +0200

    add: started learning about Orthodox Canonical Form

---
+++ 02/ex00/Fixed.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glacroix <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/03 19:00:02 by glacroix          #+#    #+#             */
+/*   Updated: 2024/05/03 19:02:55 by glacroix         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+	int number;
+	const int fractional;
+		public:
+	void setRawBits(int const raw);
+	int	getRawBits(void);
+}
+
+#endif
---
+++ 02/ex00/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glacroix <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/03 18:57:32 by glacroix          #+#    #+#             */
+/*   Updated: 2024/05/03 18:58:48 by glacroix         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main( void ) 
+{
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0
+}	

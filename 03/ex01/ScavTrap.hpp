cea5e8de4f58eba25bfe0eff54c28e76378709a3
PGCL1 <earlybirdcatchestheworm1@gmail.com>
2024-06-03 19:02:02 +0200

    add: base structure for ex01

---
+++ 03/ex01/ScavTrap.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glacroix <glacroix>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/03 18:10:59 by glacroix          #+#    #+#             */
+/*   Updated: 2024/06/03 18:19:08 by glacroix         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : private ClapTrap
+{
+    public:
+        void guardGate();
+};
+
+# endif

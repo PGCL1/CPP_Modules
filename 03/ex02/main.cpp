579a7c1456e5d57928be7f662091f6e7611a8289
PGCL1 <earlybirdcatchestheworm1@gmail.com>
2024-06-10 18:55:55 +0200

    add: updated main for ex02

--- 03/ex02/main.cpp
+++ 03/ex02/main.cpp
@@ -6,28 +6,28 @@
 /*   By: glacroix <glacroix>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 16:22:22 by glacroix          #+#    #+#             */
-/*   Updated: 2024/06/05 18:13:17 by glacroix         ###   ########.fr       */
+/*   Updated: 2024/06/10 18:54:51 by glacroix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 
 int main()
 {
-    std::cout << ROSE << "\nI will create: Adri ScavTrap" << RESET << std::endl; 
-    ScavTrap obj("Adri");
+    std::cout << ROSE << "\nI will create: Adri FragTrap" << RESET << std::endl; 
+    FragTrap obj("Adri");
 
     //obj.print();
     obj.attack("a big boar");
-    obj.guardGate();
+    obj.highFivesGuys();
 
-    std::cout << ROSE << "\nI will create: Maria ScavTrap" << RESET << std::endl; 
-    ScavTrap another("Maria");
+    std::cout << ROSE << "\nI will create: Maria FragTrap" << RESET << std::endl; 
+    FragTrap another("Maria");
     another = obj;
     another.print();
 
-    std::cout << ROSE << "\nI will create: a Adri ScavTrap clone" << RESET << std::endl; 
-    ScavTrap AdriClone(obj);
+    std::cout << ROSE << "\nI will create: a Adri FragTrap clone" << RESET << std::endl; 
+    FragTrap AdriClone(obj);
     AdriClone.print();
 
     return (0);

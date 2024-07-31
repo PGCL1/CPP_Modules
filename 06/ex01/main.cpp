d4f9204b2201d57d653c77c03b3bcba44904855d
PGCL1 <earlybirdcatchestheworm1@gmail.com>
2024-07-31 15:22:47 +0200

    ex01: changed remote directory to local's (more updated)

--- 06/ex01/main.cpp
+++ 06/ex01/main.cpp
@@ -13,10 +13,37 @@
 #include <iostream>
 #include "Data.hpp"
 #include "Serializer.hpp"
-#include <assert.h>
+#include <stdint.h>
+#include <cstring>
+
+/*struct Test
+{
+    int a;
+    int b;
+    int c;
+};*/
+
+struct Test
+{
+    char a;
+    char b;
+    char c;
+    char d;
+};
+
 
 int main()
 {
+    // 0 0 00000101 00111001
+    // rw -> [][][][] [][][][]
+    int rw = 1337;
+    Test t = *(Test*)&rw;
+
+    std::cout << (int)t.a << std::endl;
+
+
+    return 0;
+#if 0
       Data myStruct;
       //void *random;
  
@@ -26,7 +53,9 @@ int main()
     Data* anotherStruct = reinterpret_cast<Data*>(value);
     std::cout << "The address of myStruct is: " << &myStruct << std::endl;
     std::cout << "The address of anotherStruct is: " << anotherStruct << std::endl;
-    std::cout << "If the addresses are identical, memcmp will give 0 (see below)\n\tResult: " 
-        << memcmp(anotherStruct, &myStruct, sizeof(*anotherStruct)) << std::endl;
+    std::cout << "Result: " << (memcmp(anotherStruct, &myStruct, sizeof(*anotherStruct)) == 0
+                ? "Addresses are identical"
+                : "Addresses are different") << std::endl;
+#endif
     return 0;
 }

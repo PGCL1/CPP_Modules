e28cb78f02545a23db757a8aaf5401d768959423
PGCL1 <earlybirdcatchestheworm1@gmail.com>
2024-08-02 12:53:48 +0200

    ex00: created all 3 functions (min, max, swap)

--- 07/ex00/main.cpp
+++ 07/ex00/main.cpp
@@ -6,36 +6,32 @@
 /*   By: glacroix <PGCL>                            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 20:18:30 by glacroix          #+#    #+#             */
-/*   Updated: 2024/08/01 20:24:48 by glacroix         ###   ########.fr       */
+/*   Updated: 2024/08/02 12:52:31 by glacroix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Swap.hpp"
+#include "Min.hpp"
+#include "Max.hpp"
 #include <iostream>
-
-template <typename T>
-void swap(T& a, T& b)
-{
-    T temp = a;
-    a = b;
-    b = temp;
-}
-
-
 
 int main()
 {
-    int a = 14;
-    int b = 28;
-    std::cout << "a = " << a << " | b = " << b << std::endl;
-    ::swap(a, b);
-    std::cout << "a = " << a << " | b = " << b << std::endl;
+    int a = 2;
+    int b = 3;
 
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << "\n" << std::endl;
+    
+    std::string c = "chaine1";
+    std::string d = "chaine2";
 
-    std::string str1 = "salut";
-    std::string str2 = "la miss";
-    std::cout << "str1 = " << str1 << " | str2 = " << str2 << std::endl;
-    ::swap(str1, str2);
-    std::cout << "str1 = " << str1 << " | str2 = " << str2 << std::endl;
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
     return 0;
 }
 

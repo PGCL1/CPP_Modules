fe20b4621ddb2ee078ce017ad256d1473e616f8b
PGCL1 <earlybirdcatchestheworm1@gmail.com>
2024-08-05 13:05:21 +0200

    ex02: created empty and full arrays

--- 07/ex02/Array.hpp
+++ 07/ex02/Array.hpp
@@ -3,28 +3,89 @@
 /*                                                        :::      ::::::::   */
 /*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: glacroix <PGCL>                            +#+  +:+       +#+        */
+/*   By: glacroix <glacroix@student.42madrid.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 15:01:37 by glacroix          #+#    #+#             */
-/*   Updated: 2024/08/02 15:10:54 by glacroix         ###   ########.fr       */
+/*   Updated: 2024/08/05 12:33:00 by glacroix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
-template <typename T>
+#include <iostream>
+
+template <class T>
 class Array
 {
     private:
-       T m_element;
+        T *m_element;
     public:
-       Array();
-       Array(unsigned int n);
-       Array(Array const& example);
-       Array& operator=(Array const& example);
-       ~Array();
+        void    setElement(int pos, T value);
+        T       getElement(int pos);
+        size_t  size();
+
+        Array();
+        Array(unsigned int n);
+        Array(Array const& example);
+        Array& operator=(Array const& example)
+        {
+            std::cout << "Array's Assignement Overload `=`" << std::endl;
+            if (this != example)
+            {
+                for (unsigned int i = 0; example[i]; i++)
+                    this->m_element[i] = example.m_element[i];
+            }
+            return (*this);
+        }
+        ~Array();
 };
+
+template <class T>
+Array<T>::Array() 
+{
+    std::cout << "Array's Default Constructor" << std::endl;
+    m_element = new T();
+}
+
+template <class T>
+Array<T>::Array(unsigned int n)
+{
+    std::cout << "Array's Param Constructor" << std::endl;
+    m_element = new T[n]();
+}
+
+template <class T>
+Array<T>::Array(Array<T> const& example)
+{
+    //need to find a way to find len
+    if (this != &example)
+    {
+        //this->m_element = new T[len];
+        for (unsigned int i = 0; example.m_element[i]; i++)
+            this->m_element[i] = example.m_element[i];
+    }
+    return;
+}
+
+template <class T>
+Array<T>::~Array()
+{
+    std::cout << "Array's Default Destructor" << std::endl;
+    delete [] m_element;
+}
+
+template <class T>
+void Array<T>::setElement(int pos, T value)
+{
+    m_element[pos] = value;
+}
+
+template <class T>
+T Array<T>::getElement(int pos)
+{
+    return (m_element[pos]);
+}
 
 #endif
 
--- 07/ex02/main.cpp
+++ 07/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: glacroix <PGCL>                            +#+  +:+       +#+        */
+/*   By: glacroix <glacroix@student.42madrid.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 14:54:40 by glacroix          #+#    #+#             */
-/*   Updated: 2024/08/02 15:14:47 by glacroix         ###   ########.fr       */
+/*   Updated: 2024/08/05 12:42:11 by glacroix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,17 @@
 
 int main()
 {
+#if 1
+    Array<int> a;
+    std::cout << a.getElement(0) << std::endl;
 
-    return 0;
+    Array<int> b(10);
+    std::cout << b.getElement(0) << std::endl;
+#else
+
+    int *a = new int();
+    std::cout << a[0] << std::endl;
+
+#endif
 }
 

e2ebfca67d9fb0c737a2e35a12e6fb2215876ebb
PGCL1 <earlybirdcatchestheworm1@gmail.com>
2024-08-14 17:37:52 +0200

    ex02: problem in printElement, tomorrow is another day, gggg

--- 08/ex01/Span.cpp
+++ 08/ex01/Span.cpp
@@ -6,12 +6,42 @@
 /*   By: glacroix <PGCL>                            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 15:13:58 by glacroix          #+#    #+#             */
-/*   Updated: 2024/08/14 15:19:48 by glacroix         ###   ########.fr       */
+/*   Updated: 2024/08/14 17:33:28 by glacroix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 #include <iostream>
 
+Span::Span() : m_element()
+{
+    m_maxSize = 1;
+    std::cout << "Default Constructor Span" << std::endl;
+    
+}
 
+Span::Span(unsigned int N) : m_element(N)
+{
+    m_maxSize = N;
+    std::cout << "Constructor with Params Span" << std::endl;
+}
 
+Span::~Span()
+{
+    std::cout << "Default Destructor Span" << std::endl;
+}
+
+void Span::addNumber(int num)
+{
+   m_element.push_back(num); 
+   m_element.sort();
+}
+
+void Span::printElements()
+{
+    std::list<int>::iterator it = m_element.begin();
+    std::list<int>::iterator end = m_element.end();
+    while (it != end)
+        std::cout << (*it)++ << " ";
+    std::cout << std::endl;
+}
--- 08/ex01/Span.hpp
+++ 08/ex01/Span.hpp
@@ -6,22 +6,28 @@
 /*   By: glacroix <PGCL>                            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 15:07:09 by glacroix          #+#    #+#             */
-/*   Updated: 2024/08/14 15:13:43 by glacroix         ###   ########.fr       */
+/*   Updated: 2024/08/14 17:35:41 by glacroix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SPAN_HPP
 #define SPAN_HPP
 
+#include <vector>
+#include <list>
+
 class Span {
     private:
-        int *element;
+        std::list<int> m_element;
+        unsigned int m_maxSize;
     public:
-        void addNumber();
-        void addAll();
-        int shortestSpan();
-        int longestSpan();
+        void addNumber(int num);
+        void printElements();
+        //void addAll(std::vector<int> argList);
+        //int shortestSpan();
+        //int longestSpan();
 
+        Span();
         Span(unsigned int N);
         //copy and = overload
         ~Span();

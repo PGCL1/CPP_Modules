f2af3725af9ec549f6767392ba14cda42f5c8ac8
PGCL1 <earlybirdcatchestheworm1@gmail.com>
2024-08-27 17:08:20 +0200

    ex02: weird, i think i understand the FD algo, maybe i misunderstood something, thoughts in test.cpp

--- 09/ex02/test.cpp
+++ 09/ex02/test.cpp
@@ -6,7 +6,7 @@
 /*   By: glacroix <PGCL>                            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 16:37:08 by glacroix          #+#    #+#             */
-/*   Updated: 2024/08/27 16:53:34 by glacroix         ###   ########.fr       */
+/*   Updated: 2024/08/27 17:06:11 by glacroix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,13 @@
  *
  * size = 5
  * pairs = (3, 5) (9, 7) (4)
+ * L  = (3, 7)
  * larg el = 5      9     4
- * S =       (4, 5, 9)
- * 
-
+ * container(sorted larg el) =  (4, 5, 9)
+ * container = (3, 4, 5, 9)
+ * i guess use the function whatsMyPosition to insert whatever's left in pairs not in S
+ *  - find the position of L[x] in S and insert it there
+ *  - repeat until no elements left in L
+ *
+ *
+*/
